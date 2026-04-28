681b60f97660a0259b22f317968670a5ef2748e4
smsadat1 <smsadat788@gmail.com>
2026-04-28 11:47:59 +0600

    feat(query): Implement .first() and .count() method

--- sql/query.py
+++ sql/query.py
@@ -11,15 +11,35 @@ class Query:
         self.conn = Connection(DB_PATH)
         self.exec = Executor(self.conn)
         self.table = table
+
         self._where = None
         self._params = []
         self._limits = None
 
+
     def __iter__(self):
         sql, params = self.compile()
         cursor = iter(self.exec.fetchall(sql, params))
+
         for row in cursor:
             yield row
+
+
+    def first(self):
+        sql, params = self.compile()
+        sql += ' LIMIT 1'
+        return self.exec.fetchone(sql, params)
+
+
+    def count(self):
+        sql = f'SELECT COUNT(*) FROM {self.table}'
+
+        if self._where:
+            sql += f' WHERE {self._where}'
+
+        row = self.exec.fetchone(sql, self._params)
+        return row[0]
+
 
     def where(self, condition: str, params=None):
         self._where = condition
@@ -27,9 +47,11 @@ class Query:
             self._params.extend(params)
         return self
 
+
     def limit(self, n: int):
         self._limits = n
         return self
+
 
     def compile(self):
         sql = f'SELECT * FROM {self.table}'
